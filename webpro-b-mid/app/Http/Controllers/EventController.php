1d86104c6573fa34ec60667e82781ddf03b2f8be
Emmanuel Maximus Yohanes <geofanny.emmanuel@gmail.com>
2020-10-26 15:27:59 +0700

    event controller

---
+++ webpro-b-mid/app/Http/Controllers/EventController.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Event;
+use Illuminate\Http\Request;
+
+class EventController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $events = Event::latest()->paginate(5);
+
+        return view('event.index', compact('events'))->with('i', (request()->input('page', 1) - 1) * 5);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('event.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'event_name' => 'required',
+            'event_price' => 'required',
+            'event_start' => 'required',
+            'event_end' => 'required',
+        ]);
+
+        Event::create($request->all());
+
+        return redirect()->route('event.index')->with('success', 'Event created successfully.');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Event  $event
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Event $event)
+    {
+        return view('event.show', compact('event'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Event  $event
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Event $event)
+    {
+        return view('event.edit', compact('event'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Event  $event
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Event $event)
+    {
+        $request->validate([
+            'event_name' => 'required',
+            'event_price' => 'required',
+            'event_start' => 'required',
+            'event_end' => 'required',
+        ]);
+        $event.update($request->all());
+        return redirect()->route('event.index')->with('success', 'Event updated successfully');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Event  $event
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Event $event)
+    {
+        $event->delete();
+        return redirect()->route('event.index')->with('success', 'Event deleted successfully');
+    }
+}
