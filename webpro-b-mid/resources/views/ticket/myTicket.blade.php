50c54ce87549f24e1b94363141066b8f88f6e65b
Emmanuel Maximus Yohanes <geofanny.emmanuel@gmail.com>
2020-10-29 12:08:34 +0700

    other UI improvements

--- webpro-b-mid/resources/views/ticket/myTicket.blade.php
+++ webpro-b-mid/resources/views/ticket/myTicket.blade.php
@@ -2,29 +2,29 @@
 
 @section('content')
 <div class="container">
-    <div class="card-body">
-        <table class="table table-striped">
-            <thead class="text-left thead-dark">
-                <th>Code</th>
-                <th>Name</th>
-                <!-- <th class="col-xl">Jawaban</th> -->
-                <th>Purchase Date</th>
-                <th>Used</th>
-            </thead>
-            <tbody>
-                @foreach($my_ticket as $ticket => $t)
-                <tr class="text-left">
-                    <td>{{$t->ticket_id}}</td>
-                    <td>{{$t->event_name}}</td>
-                    <td>{{$t->updated_at}}</td>
-                    <td>{{$t->used}}</td>
-                </tr>
-                @endforeach
-            </tbody>
-        </table>
+    <div class="row justify-content-center">
+        <div class="col-md-10">
+            <table class="table table-striped">
+                <thead class="text-left thead-dark">
+                    <th>Code</th>
+                    <th>Name</th>
+                    <!-- <th class="col-xl">Jawaban</th> -->
+                    <th>Purchase Date</th>
+                    <th>Used</th>
+                </thead>
+                <tbody>
+                    @foreach($my_ticket as $ticket => $t)
+                    <tr class="text-left">
+                        <td>{{$t->ticket_id}}</td>
+                        <td>{{$t->event_name}}</td>
+                        <td>{{$t->updated_at}}</td>
+                        <td>{{$t->used}}</td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+            <a class="btn btn-danger px-5" href="{{ url()->previous() }}">Back</a>
+        </div>
     </div>
-
-    <!-- <a class="btn btn-primary" href="{{'my-tickets'}}">My Tickets</a> -->
-    <a class="btn btn-primary" href="{{'my-events'}}">My Events</a>
 </div>
 @endsection
